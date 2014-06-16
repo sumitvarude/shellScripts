99ea9146f0481da67d830cd932a78f379317448e
Sumit Varude <sumitvarude@gmail.com>
2014-06-16 16:11:40 +0530

    created wifi.sh for turn ON/OFF wifi

---
+++ wifi.sh
@@ -0,0 +1,14 @@
+CURRENT_DEVICE=$(networksetup -listallhardwareports | awk '$3=="Wi-Fi" {getline; print $2}')
+echo "Current Wi-Fi Device = '$CURRENT_DEVICE'"
+networksetup -setairportpower $CURRENT_DEVICE "$1"
+if [ "$1" = "on" ]
+then
+    echo "Wifi is ON now"
+    sleep 5
+    bash start.sh
+else
+    echo "Closing firefox"
+    sleep 1
+    pkill -f firefox
+    echo "wifi is OFF now"
+fi
